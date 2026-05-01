72108d60a55e6979b3e1868c09cf6be1bf091223
Jirawat Siripuk <siripukwaewwrrn@gmail.com>
2026-05-01 17:22:35 +0700

    example: update basic arithmetic example

--- examples/basic_arithmetic.c
+++ examples/basic_arithmetic.c
@@ -5,7 +5,7 @@
 #include "../simd_f128_io.h"
 
 int main() {
-    printf("--- simd-f128 basic arithmetic ---\n\n");
+    printf("--- simd-fp basic arithmetic ---\n\n");
 
     simd_f128 a = SIMD_F128_PI;   /* pi  ~ 3.14159265358979323846... */
     simd_f128 b = SIMD_F128_E;    /* e   ~ 2.71828182845904523536... */
