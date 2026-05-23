056342fbc642752ae851a99352ff1176985f9c2a
Jirawat Siripuk <siripukwaewwrrn@gmail.com>
2026-05-23 09:55:06 +0700

    style: update comments in basic_arithmetic example

--- examples/basic_arithmetic.c
+++ examples/basic_arithmetic.c
@@ -7,8 +7,8 @@
 int main() {
     printf("--- simd-fp basic arithmetic ---\n\n");
 
-    simd_f128 a = SIMD_F128_PI;   /* pi  ~ 3.14159265358979323846... */
-    simd_f128 b = SIMD_F128_E;    /* e   ~ 2.71828182845904523536... */
+    simd_f128 a = SIMD_F128_PI;   // pi  ~ 3.14159265358979323846...
+    simd_f128 b = SIMD_F128_E;    // e   ~ 2.71828182845904523536...
 
     simd_f128 sum  = simd_f128_add(a, b);
     simd_f128 diff = simd_f128_sub(a, b);
