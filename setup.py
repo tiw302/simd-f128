5f6e5391700bde725b7fb2551387ab99e0e19316
Jirawat Siripuk <siripukwaewwrrn@gmail.com>
2026-05-23 09:55:52 +0700

    chore: add setup.py for Python extension building

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, Extension
+import pybind11
+
+ext_modules = [
+    Extension(
+        'simd_f128',
+        ['python/simd_f128_python.cpp'],
+        include_dirs=[pybind11.get_include(), '.'],
+        language='c++',
+        extra_compile_args=['-O3', '-march=native', '-std=c++14', '-DSIMD_F128_EXCEPTIONS']
+    ),
+]
+
+setup(
+    name='simd-f128',
+    version='1.0.0',
+    description="High-performance 128-bit Double-Double arithmetic",
+    ext_modules=ext_modules,
+    packages=[],
+    zip_safe=False,
+)
