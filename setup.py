cc3665f3ac86ae4868724735de6497f82c05b301
Jirawat Siripuk <siripukwaewwrrn@gmail.com>
2026-05-29 19:49:45 +0700

    chore: bump version to 1.2.4

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ ext_modules = [
 
 setup(
     name='simd-f128',
-    version='1.2.2',
+    version='1.2.4',
     description="High-performance 128-bit Double-Double arithmetic",
     ext_modules=ext_modules,
     packages=[],
