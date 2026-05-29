e4fc8f2cc47846a39c3f8abb189a20337d7f1fee
Jirawat Siripuk <siripukwaewwrrn@gmail.com>
2026-05-29 17:16:53 +0700

    build(wasm): export simd_f128_wasm_get_low in emcc

--- js/build_wasm.sh
+++ js/build_wasm.sh
@@ -11,7 +11,7 @@ then
 fi
 
 emcc wasm_wrapper.c -O3 -s WASM=1 \
-    -s EXPORTED_FUNCTIONS="['_simd_f128_wasm_add', '_simd_f128_wasm_sub', '_simd_f128_wasm_mul', '_simd_f128_wasm_div', '_simd_f128_wasm_from_string', '_simd_f128_wasm_to_string', '_malloc', '_free']" \
+    -s EXPORTED_FUNCTIONS="['_simd_f128_wasm_add', '_simd_f128_wasm_sub', '_simd_f128_wasm_mul', '_simd_f128_wasm_div', '_simd_f128_wasm_get_low', '_simd_f128_wasm_from_string', '_simd_f128_wasm_to_string', '_malloc', '_free']" \
     -s EXPORTED_RUNTIME_METHODS="['ccall', 'cwrap', 'UTF8ToString', 'getValue', 'setValue']" \
     -o simd_f128_wasm.js
 
