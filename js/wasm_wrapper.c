4025fb1bb92dde7392f89d8a6069f36197d4d56b
Jirawat Siripuk <siripukwaewwrrn@gmail.com>
2026-05-29 17:17:04 +0700

    feat(wasm): refactor wrapper to pass doubles directly and add sub/div

--- js/wasm_wrapper.c
+++ js/wasm_wrapper.c
@@ -8,37 +8,51 @@
 extern "C" {
 #endif
 
-/* 
- * we use an array of 2 doubles [hi, lo] to communicate seamlessly 
- * with javascript's float64array over wasm memory.
- */
+// Global/static variable to store the low double of the last calculation
+double wasm_low_result = 0.0;
 
-void simd_f128_wasm_add(const double* a, const double* b, double* out) {
-    simd_f128 va = {a[0], a[1]};
-    simd_f128 vb = {b[0], b[1]};
+double simd_f128_wasm_add(double a_hi, double a_lo, double b_hi, double b_lo) {
+    simd_f128 va = simd_f128_from_hi_lo(a_hi, a_lo);
+    simd_f128 vb = simd_f128_from_hi_lo(b_hi, b_lo);
     simd_f128 res = simd_f128_add(va, vb);
-    simd_f128_extract(res, &out[0], &out[1]);
+    double r_hi, r_lo;
+    simd_f128_extract(res, &r_hi, &r_lo);
+    wasm_low_result = r_lo;
+    return r_hi;
 }
 
-void simd_f128_wasm_mul(const double* a, const double* b, double* out) {
-    simd_f128 va = {a[0], a[1]};
-    simd_f128 vb = {b[0], b[1]};
-    simd_f128 res = simd_f128_mul(va, vb);
-    simd_f128_extract(res, &out[0], &out[1]);
-}
-
-void simd_f128_wasm_sub(const double* a, const double* b, double* out) {
-    simd_f128 va = {a[0], a[1]};
-    simd_f128 vb = {b[0], b[1]};
+double simd_f128_wasm_sub(double a_hi, double a_lo, double b_hi, double b_lo) {
+    simd_f128 va = simd_f128_from_hi_lo(a_hi, a_lo);
+    simd_f128 vb = simd_f128_from_hi_lo(b_hi, b_lo);
     simd_f128 res = simd_f128_sub(va, vb);
-    simd_f128_extract(res, &out[0], &out[1]);
+    double r_hi, r_lo;
+    simd_f128_extract(res, &r_hi, &r_lo);
+    wasm_low_result = r_lo;
+    return r_hi;
 }
 
-void simd_f128_wasm_div(const double* a, const double* b, double* out) {
-    simd_f128 va = {a[0], a[1]};
-    simd_f128 vb = {b[0], b[1]};
+double simd_f128_wasm_mul(double a_hi, double a_lo, double b_hi, double b_lo) {
+    simd_f128 va = simd_f128_from_hi_lo(a_hi, a_lo);
+    simd_f128 vb = simd_f128_from_hi_lo(b_hi, b_lo);
+    simd_f128 res = simd_f128_mul(va, vb);
+    double r_hi, r_lo;
+    simd_f128_extract(res, &r_hi, &r_lo);
+    wasm_low_result = r_lo;
+    return r_hi;
+}
+
+double simd_f128_wasm_div(double a_hi, double a_lo, double b_hi, double b_lo) {
+    simd_f128 va = simd_f128_from_hi_lo(a_hi, a_lo);
+    simd_f128 vb = simd_f128_from_hi_lo(b_hi, b_lo);
     simd_f128 res = simd_f128_div(va, vb);
-    simd_f128_extract(res, &out[0], &out[1]);
+    double r_hi, r_lo;
+    simd_f128_extract(res, &r_hi, &r_lo);
+    wasm_low_result = r_lo;
+    return r_hi;
+}
+
+double simd_f128_wasm_get_low() {
+    return wasm_low_result;
 }
 
 void simd_f128_wasm_from_string(const char* str, double* out) {
@@ -46,8 +60,8 @@ void simd_f128_wasm_from_string(const char* str, double* out) {
     simd_f128_extract(res, &out[0], &out[1]);
 }
 
-void simd_f128_wasm_to_string(const double* a, char* buf, int buf_size) {
-    simd_f128 va = {a[0], a[1]};
+void simd_f128_wasm_to_string(double a_hi, double a_lo, char* buf, int buf_size) {
+    simd_f128 va = simd_f128_from_hi_lo(a_hi, a_lo);
     simd_f128_to_string(buf, buf_size, va);
 }
 
