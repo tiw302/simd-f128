f1d6a6ed80c03ccb57cf25e418962d243e812fe7
Jirawat Siripuk <siripukwaewwrrn@gmail.com>
2026-06-12 06:54:53 +0700

    feat(complex): add C functions for division, abs, phase, and conjugate
    * Implement complex division simd_f128_complex_div using conjugate multiplication
    * Implement complex magnitude estimation functions: simd_f128_complex_abs and simd_f128_complex_arg
    * Implement complex conjugate function simd_f128_complex_conj
    * Add header documentation comments for all complex operations

--- include/simd_f128_complex.h
+++ include/simd_f128_complex.h
@@ -1,9 +1,10 @@
-// updated 2026-05-23
+// updated 2026-06-06
 
 #ifndef SIMD_F128_COMPLEX_H
 #define SIMD_F128_COMPLEX_H
 
 #include "simd_f128.h"
+#include "simd_f128_math.h"
 
 
 //  ██████  ██████  ███    ███ ██████  ██      ███████ ██   ██ 
@@ -23,6 +24,7 @@ typedef struct {
     simd_f128 imag;
 } simd_f128_complex;
 
+// add two complex numbers
 SIMD_F128_INLINE simd_f128_complex simd_f128_complex_add(simd_f128_complex a, simd_f128_complex b) {
     simd_f128_complex res;
     res.real = simd_f128_add(a.real, b.real);
@@ -30,6 +32,7 @@ SIMD_F128_INLINE simd_f128_complex simd_f128_complex_add(simd_f128_complex a, si
     return res;
 }
 
+// subtract two complex numbers
 SIMD_F128_INLINE simd_f128_complex simd_f128_complex_sub(simd_f128_complex a, simd_f128_complex b) {
     simd_f128_complex res;
     res.real = simd_f128_sub(a.real, b.real);
@@ -37,8 +40,9 @@ SIMD_F128_INLINE simd_f128_complex simd_f128_complex_sub(simd_f128_complex a, si
     return res;
 }
 
+// multiply two complex numbers using the formula:
+// (r1 + i1*i) * (r2 + i2*i) = (r1*r2 - i1*i2) + (r1*i2 + r2*i1)*i
 SIMD_F128_INLINE simd_f128_complex simd_f128_complex_mul(simd_f128_complex a, simd_f128_complex b) {
-    // (r1+i1*i) * (r2+i2*i)
     simd_f128_complex res;
     simd_f128 r1r2 = simd_f128_mul(a.real, b.real);
     simd_f128 i1i2 = simd_f128_mul(a.imag, b.imag);
@@ -50,13 +54,41 @@ SIMD_F128_INLINE simd_f128_complex simd_f128_complex_mul(simd_f128_complex a, si
     return res;
 }
 
-// |z|^2 (good for mandelbrot checks)
+// divide two complex numbers: (a * conj(b)) / |b|^2
+SIMD_F128_INLINE simd_f128_complex simd_f128_complex_div(simd_f128_complex a, simd_f128_complex b) {
+    simd_f128 d = simd_f128_add(simd_f128_mul(b.real, b.real), simd_f128_mul(b.imag, b.imag));
+    simd_f128_complex res;
+    res.real = simd_f128_div(simd_f128_add(simd_f128_mul(a.real, b.real), simd_f128_mul(a.imag, b.imag)), d);
+    res.imag = simd_f128_div(simd_f128_sub(simd_f128_mul(a.imag, b.real), simd_f128_mul(a.real, b.imag)), d);
+    return res;
+}
+
+// calculate absolute square magnitude: |z|^2 = real^2 + imag^2
+// optimized for fractal rendering escape time checks (saves a square root)
 SIMD_F128_INLINE simd_f128 simd_f128_complex_abs_sqr(simd_f128_complex a) {
     return simd_f128_add(simd_f128_mul(a.real, a.real), simd_f128_mul(a.imag, a.imag));
+}
+
+// calculate the magnitude (absolute value) of a complex number
+SIMD_F128_INLINE simd_f128 simd_f128_complex_abs(simd_f128_complex a) {
+    return simd_f128_sqrt(simd_f128_complex_abs_sqr(a));
+}
+
+// calculate the phase angle (argument) of a complex number
+SIMD_F128_INLINE simd_f128 simd_f128_complex_arg(simd_f128_complex a) {
+    return simd_f128_atan2(a.imag, a.real);
+}
+
+// calculate the complex conjugate
+SIMD_F128_INLINE simd_f128_complex simd_f128_complex_conj(simd_f128_complex a) {
+    simd_f128_complex res;
+    res.real = a.real;
+    res.imag = simd_f128_neg(a.imag);
+    return res;
 }
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif // simd_f128_complex_h
+#endif // SIMD_F128_COMPLEX_H
