0601c09d26ce5f9db7377a9dee093f504f957012
Jirawat Siripuk <siripukwaewwrrn@gmail.com>
2026-06-12 06:55:53 +0700

    feat(js): remove global results from wasm wrappers and add new operations
    * Refactor all wrapper signatures to take a double* output parameter instead of using wasm_low_result
    * Implement WASM_BIN_OP and WASM_UN_OP macros for unified binding generation
    * Bind new mathematical operations: pow, fmod, atan2, tan, sinh, cosh, tanh, sincos, isinf, isnan, and cmp

--- js/wasm_wrapper.c
+++ js/wasm_wrapper.c
@@ -1,7 +1,10 @@
+// updated 2026-06-06
+
 #define SIMD_F128_IMPLEMENTATION
 #include "../include/simd_f128.h"
 #include "../include/simd_f128_io.h"
 #include "../include/simd_f128_math.h"
+#include "../include/simd_f128_utils.h"
 #include <stdlib.h>
 #include <string.h>
 
@@ -9,97 +12,45 @@
 extern "C" {
 #endif
 
-// Global/static variable to store the low double of the last calculation
-double wasm_low_result = 0.0;
+// no more global state. passing pointer from js to avoid race conditions.
 
-double simd_f128_wasm_add(double a_hi, double a_lo, double b_hi, double b_lo) {
-    simd_f128 va = simd_f128_from_hi_lo(a_hi, a_lo);
-    simd_f128 vb = simd_f128_from_hi_lo(b_hi, b_lo);
-    simd_f128 res = simd_f128_add(va, vb);
-    double r_hi, r_lo;
-    simd_f128_extract(res, &r_hi, &r_lo);
-    wasm_low_result = r_lo;
-    return r_hi;
-}
+#define WASM_BIN_OP(name, func) \
+    void simd_f128_wasm_##name(double a_hi, double a_lo, double b_hi, double b_lo, double* out) { \
+        simd_f128 res = func(simd_f128_from_hi_lo(a_hi, a_lo), simd_f128_from_hi_lo(b_hi, b_lo)); \
+        simd_f128_extract(res, &out[0], &out[1]); \
+    }
 
-double simd_f128_wasm_sub(double a_hi, double a_lo, double b_hi, double b_lo) {
-    simd_f128 va = simd_f128_from_hi_lo(a_hi, a_lo);
-    simd_f128 vb = simd_f128_from_hi_lo(b_hi, b_lo);
-    simd_f128 res = simd_f128_sub(va, vb);
-    double r_hi, r_lo;
-    simd_f128_extract(res, &r_hi, &r_lo);
-    wasm_low_result = r_lo;
-    return r_hi;
-}
+#define WASM_UN_OP(name, func) \
+    void simd_f128_wasm_##name(double a_hi, double a_lo, double* out) { \
+        simd_f128 res = func(simd_f128_from_hi_lo(a_hi, a_lo)); \
+        simd_f128_extract(res, &out[0], &out[1]); \
+    }
 
-double simd_f128_wasm_mul(double a_hi, double a_lo, double b_hi, double b_lo) {
-    simd_f128 va = simd_f128_from_hi_lo(a_hi, a_lo);
-    simd_f128 vb = simd_f128_from_hi_lo(b_hi, b_lo);
-    simd_f128 res = simd_f128_mul(va, vb);
-    double r_hi, r_lo;
-    simd_f128_extract(res, &r_hi, &r_lo);
-    wasm_low_result = r_lo;
-    return r_hi;
-}
+WASM_BIN_OP(add, simd_f128_add)
+WASM_BIN_OP(sub, simd_f128_sub)
+WASM_BIN_OP(mul, simd_f128_mul)
+WASM_BIN_OP(div, simd_f128_div)
+WASM_BIN_OP(pow, simd_f128_pow)
+WASM_BIN_OP(fmod, simd_f128_fmod)
+WASM_BIN_OP(atan2, simd_f128_atan2)
 
-double simd_f128_wasm_div(double a_hi, double a_lo, double b_hi, double b_lo) {
-    simd_f128 va = simd_f128_from_hi_lo(a_hi, a_lo);
-    simd_f128 vb = simd_f128_from_hi_lo(b_hi, b_lo);
-    simd_f128 res = simd_f128_div(va, vb);
-    double r_hi, r_lo;
-    simd_f128_extract(res, &r_hi, &r_lo);
-    wasm_low_result = r_lo;
-    return r_hi;
-}
-
-double simd_f128_wasm_exp(double a_hi, double a_lo) {
-    simd_f128 va = simd_f128_from_hi_lo(a_hi, a_lo);
-    simd_f128 res = simd_f128_exp(va);
-    double r_hi, r_lo;
-    simd_f128_extract(res, &r_hi, &r_lo);
-    wasm_low_result = r_lo;
-    return r_hi;
-}
-
-double simd_f128_wasm_log(double a_hi, double a_lo) {
-    simd_f128 va = simd_f128_from_hi_lo(a_hi, a_lo);
-    simd_f128 res = simd_f128_log(va);
-    double r_hi, r_lo;
-    simd_f128_extract(res, &r_hi, &r_lo);
-    wasm_low_result = r_lo;
-    return r_hi;
-}
-
-double simd_f128_wasm_sin(double a_hi, double a_lo) {
-    simd_f128 va = simd_f128_from_hi_lo(a_hi, a_lo);
-    simd_f128 res = simd_f128_sin(va);
-    double r_hi, r_lo;
-    simd_f128_extract(res, &r_hi, &r_lo);
-    wasm_low_result = r_lo;
-    return r_hi;
-}
-
-double simd_f128_wasm_cos(double a_hi, double a_lo) {
-    simd_f128 va = simd_f128_from_hi_lo(a_hi, a_lo);
-    simd_f128 res = simd_f128_cos(va);
-    double r_hi, r_lo;
-    simd_f128_extract(res, &r_hi, &r_lo);
-    wasm_low_result = r_lo;
-    return r_hi;
-}
-
-double simd_f128_wasm_sqrt(double a_hi, double a_lo) {
-    simd_f128 va = simd_f128_from_hi_lo(a_hi, a_lo);
-    simd_f128 res = simd_f128_sqrt(va);
-    double r_hi, r_lo;
-    simd_f128_extract(res, &r_hi, &r_lo);
-    wasm_low_result = r_lo;
-    return r_hi;
-}
-
-double simd_f128_wasm_get_low() {
-    return wasm_low_result;
-}
+WASM_UN_OP(exp, simd_f128_exp)
+WASM_UN_OP(log, simd_f128_log)
+WASM_UN_OP(sin, simd_f128_sin)
+WASM_UN_OP(cos, simd_f128_cos)
+WASM_UN_OP(sqrt, simd_f128_sqrt)
+WASM_UN_OP(abs, simd_f128_abs)
+WASM_UN_OP(floor, simd_f128_floor)
+WASM_UN_OP(ceil, simd_f128_ceil)
+WASM_UN_OP(round, simd_f128_round)
+WASM_UN_OP(trunc, simd_f128_trunc)
+WASM_UN_OP(atan, simd_f128_atan)
+WASM_UN_OP(asin, simd_f128_asin)
+WASM_UN_OP(acos, simd_f128_acos)
+WASM_UN_OP(tan, simd_f128_tan)
+WASM_UN_OP(sinh, simd_f128_sinh)
+WASM_UN_OP(cosh, simd_f128_cosh)
+WASM_UN_OP(tanh, simd_f128_tanh)
 
 void simd_f128_wasm_from_string(const char* str, double* out) {
     simd_f128 res = simd_f128_from_string(str);
@@ -107,8 +58,30 @@ void simd_f128_wasm_from_string(const char* str, double* out) {
 }
 
 void simd_f128_wasm_to_string(double a_hi, double a_lo, char* buf, int buf_size) {
-    simd_f128 va = simd_f128_from_hi_lo(a_hi, a_lo);
-    simd_f128_to_string(buf, buf_size, va);
+    simd_f128_to_string(buf, buf_size, simd_f128_from_hi_lo(a_hi, a_lo));
+}
+
+void simd_f128_wasm_to_string_prec(double a_hi, double a_lo, char* buf, int buf_size, int digits) {
+    simd_f128_to_string_prec(buf, buf_size, simd_f128_from_hi_lo(a_hi, a_lo), digits);
+}
+
+void simd_f128_wasm_sincos(double a_hi, double a_lo, double* out_s, double* out_c) {
+    simd_f128 s, c;
+    simd_f128_sincos(simd_f128_from_hi_lo(a_hi, a_lo), &s, &c);
+    simd_f128_extract(s, &out_s[0], &out_s[1]);
+    simd_f128_extract(c, &out_c[0], &out_c[1]);
+}
+
+int simd_f128_wasm_isnan(double a_hi, double a_lo) {
+    return simd_f128_isnan(simd_f128_from_hi_lo(a_hi, a_lo));
+}
+
+int simd_f128_wasm_isinf(double a_hi, double a_lo) {
+    return simd_f128_isinf(simd_f128_from_hi_lo(a_hi, a_lo));
+}
+
+int simd_f128_wasm_cmp(double a_hi, double a_lo, double b_hi, double b_lo) {
+    return simd_f128_cmp(simd_f128_from_hi_lo(a_hi, a_lo), simd_f128_from_hi_lo(b_hi, b_lo));
 }
 
 #ifdef __cplusplus
